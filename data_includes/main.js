0efc78b84f137f425f21b98db27f63743f613431
cameliableotu <53470693+cameliableotu@users.noreply.github.com>
2019-08-21 12:45:13 +0200

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -630,6 +630,42 @@ newSelector()
 )
 .log( "ID" , getVar("ID") )
 ;
+PennController ("experiment",
+		defaultText
+	        .print(),
+		newImage ("dogshadow.jpg")
+               .print ()
+		,
+		newAudio("barking", "barking.mp3")
+		.play()
+		,
+		newText ("<p> Now let's see what the baby dragon has to say about the shadow and reward him. </p>")
+		,
+		newText ("<p> Baby Dragon: It is certain that it is the blue dog.</p>")
+		,
+	       newImage("bigapple", "bigapple.png")
+        .settings.size(200,200)
+        // .print()
+    ,
+    newImage("smallapple", "smallapple.png")
+        .settings.size(200,200)
+        // .print()
+    ,
+    newCanvas(450,200)
+        .settings.add( 0 , 0 , getImage("bigapple") )
+        .settings.add( 250 , 0 , getImage("smallapple") )
+        .print()
+	       ,
+	       // newKey("FJ")
+newSelector()
+    .settings.add( getImage("bigapple") , getImage("smallapple") )
+    .settings.keys(          "F"    ,          "J"   )
+    .settings.log()
+    .wait()
+)
+.log( "ID" , getVar("ID") )
+;
+
 
 PennController ("experiment",
 		defaultText
