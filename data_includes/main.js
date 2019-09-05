b359582d133c17937c0aa0e67d3da681d910db5c
cameliableotu <53470693+cameliableotu@users.noreply.github.com>
2019-09-05 10:56:03 +0200

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -2399,7 +2399,7 @@ PennController("experiment" ,
       newImage ("cowshadowonecowonstage", "cowshadowonecowonstage.png")
          .print()
     ,
-    newCanvas(600, 400)
+    newCanvas(500, 400)
         .settings.add( -200 , 0 , getImage("twocows") )
         .settings.add( 300, 0 , getImage("cowshadowonecowonstage") )
         .print()
